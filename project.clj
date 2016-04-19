5af3624b0ce4063717b628523ae4b890c7a6b5c8
Vee Satayamas <vsatayamas@gmail.com>
2016-04-19 08:10:50 +0000

    update project config

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject wordcut "0.1.0-SNAPSHOT"
-  :description "Thai word breaker in Clojure"
+  :description "Word breaker for ASEAN languages written in Clojure"
   :url "http://example.com/FIXME"
   :license {:name "LGPL v3"
             :url "http://www.gnu.org/licenses/lgpl.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot wordcut.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})

5712adcf0a19da92a78e3d1cfc3955cb37a9f1ee
Vee Satayamas <vsatayamas@gmail.com>
2016-05-29 12:13:18 +0700

    hash index

--- src/wordcut/dict.clj
+++ src/wordcut/dict.clj
@@ -3,11 +3,42 @@
   (:require [clojure.java.io :as io])
   (:gen-class))
 
+(defn update-index [entries index i]
+  (let [entry (nth entries i)
+        surface (:surface entry)]
+    (loop [index index e-seq (range 1 3)]
+      (if (empty? e-seq)
+        index
+        (let [e (first (e-seq))
+              key (subs 0 e surface)]
+          (recur (if (get index key)
+                   index
+                   (assoc! index key i))
+                 (rest e-seq)))))))
+
+(defn make-index-left [content]
+  (loop [index (transient (hash-map))
+         i 0]
+    (if (< i (count content))
+      (persistent! index)
+      (recur (update-index content index i)
+             (inc i)))))
+
+(defn make-index-right [content]
+  (loop [index (transient (hash-map))
+         i (dec (count content))]
+    (if (>= i 0)
+      (persistent! index)
+      (recur (update-index content index i)
+             (dec i)))))
+
 (defn read-dict [uri]
   (let [content (into-array (let [lines (str/split-lines (slurp uri))]
                               (map (fn [line] {:surface line})
                                    (sort lines))))]
     {:content content
+     :index {:left (make-index-left content)
+             :right (make-index-right content)}
      :r (dec (count content))}))
 
 (defn default-thai-dict-url []
@@ -28,12 +59,12 @@
 (defn read-default-khmer-dict []
   (read-dict (default-khmer-dict-url)))
 
-(defn dict-seek [dict
-                 policy
-                 ^Integer l
-                 ^Integer r
-                 ^Integer offset
-                 ^Character ch]
+(defn dict-seek-bsearch [dict
+                         policy
+                         ^Integer l
+                         ^Integer r
+                         ^Integer offset
+                         ^Character ch] 
   (let [content (:content dict)]
     (loop [^Integer l l ^Integer r r ^Integer ans nil]
       (if (<= l r)
@@ -49,6 +80,28 @@
                     (= policy :LEFT) (recur l (- m 1) m)
                     (= policy :RIGHT) (recur (+ 1 m) r m)))))    
         ans))))
+
+(defn dict-seek [dict
+                 policy
+                 ^Integer l
+                 ^Integer r
+                 ^Integer offset
+                 ^Character ch]
+  (cond
+    (nil? (get :index dict))
+    (dict-seek-bsearch dict policy l r offset ch)
+    (= offset 0)
+    (let [idx (get (:index dict) policy)
+          key (str ch)]
+      (get idx key))
+    (= offset 1)
+    (let [idx (get (:index dict) policy)
+          content (:content dict)
+          surface (:surface (nth content l))
+          key (str (subs surface 0 offset) ch)]
+      (get idx key))
+    :else
+    (dict-seek-bsearch dict policy l r offset ch)))
 
 (defn pointer-update [pointer ch]
   (let [p pointer

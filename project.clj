bdf2660f54420dcf302132fd32c88c6884185f4e
Vee Satayamas <vsatayamas@gmail.com>
2017-01-17 01:06:58 +0700

    v0.4.0

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject wordcut "0.3.0"
+(defproject wordcut "0.4.0"
   :description "Word breaker for ASEAN languages written in Clojure"
   :url "https://github.com/veer66/wordcut-clj"
   :license {:name "LGPL v3"
             :url "http://www.gnu.org/licenses/lgpl.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clojopts "0.3.5"]]
+                 [clojopts "0.3.5"]
+                 [net.veerkesto/prefixtree "1.0"]]
   :main ^:skip-aot wordcut.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
